0d579db9cf99a8f8a332dc07989ec021f1377db7
suresh048 <90115289+suresh048@users.noreply.github.com>
2021-10-02 14:16:49 -0500

    Create camera.java

---
+++ camera.java
@@ -0,0 +1,15 @@
+
+public class CameraContext {
+	PhoneCameraState camera;
+
+	public void setPhoneCameraState(PhoneCameraState camera) 
+	{
+		this.camera = camera;	
+	}
+	
+	public PhoneCameraState getPhoneCameraState()
+	{
+		return camera;
+	}
+
+}
